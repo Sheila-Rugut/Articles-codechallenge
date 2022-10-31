06c26d87a73b28065aba52793af30b49cebefece
Sheila <rugut.cheruto@student.moringaschool.com>
2022-10-31 23:17:35 +0300

    complete

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -10,12 +10,27 @@ class Magazine
   def self.all
     @@all
   end
-  def contributors
+  def mag_articles
     Article.all.filter{|article|
-  article.magazine.name==@name}.map{|article|article.author}.uniq
+    article.magazine.name==@name}
+  end
+  def mag_authors
+    mag_articles.map{|article|article.author}
+  end
+  def contributors
+   mag_authors.uniq
   end
   def self.find_by_name(name)
     Magazine.all.find{|magazine|
     magazine.name==name}
+  end
+  def article_titles
+    mag_articles.map{|article|article.title}.uniq
+  end
+  def contributing_authors
+    all_authors=[]
+    mag_authors.tally.each{|key, value|
+   value>2 && (all_authors<<key)}
+   all_authors
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -8,17 +8,28 @@ end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
-
+#author instances
 author1=Author.new("Author1")
+author2=Author.new("Author2")
+author3=Author.new("Author3")
+
+ #magazine instances
 magazine1=Magazine.new("Magazine1", "Category1")
 magazine2=Magazine.new("Magazine2", "Category1")
 magazine3=Magazine.new("Magazine3", "Category2")
+
+#articles instances
 article1=Article.new(author1,magazine1, "Title1")
-article2=Article.new(article1,magazine1, "Title2")
+article2=Article.new(author2,magazine2, "Title2")
+
+#add articles
 author1.add_article(magazine1, "Title3")
 author1.add_article(magazine2, "Title4")
 author1.add_article(magazine3, "Title5")
-
+author1.add_article(magazine1, "Title8")
+author2.add_article(magazine1, "Title6")
+pp author1.articles
+pp author2.articles
 
 
 
